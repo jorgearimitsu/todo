59b000d63dbd7641d3dfe964cf9feefd044c700a
Jorge Arimitsu <jorge.developer@protonmail.com>
2020-05-31 16:16:18 -0300

    Creating Tasks Component

---
+++ src/components/Tasks/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './styles.module.css'
+
+class Tasks extends Component {
+  render() {
+    return (
+      <div className={styles.tasks}>
+        <h1>Task Manager</h1>
+      </div>
+    )
+  }
+}
+
+export default Tasks
