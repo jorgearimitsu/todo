ff7248bc6cc5ce003597f3051e99c5992f1c1c58
Jorge Arimitsu <jorge.developer@protonmail.com>
2020-05-31 17:26:48 -0300

    Refactoring TaskList

--- src/components/Tasks/TaskList/index.js
+++ src/components/Tasks/TaskList/index.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-const EmptyList = () => (
-  <div className={styles.emptyList}>
-    <p>You don't have any task</p>
-  </div>
-)
-
-const FilledList = ({tasks, removeTask}) => (
-  <ul className={styles.list}>
-    {tasks.map(task => (
-      <li className={styles.task} key={task.id}>
-        {task.name}
-
-        <button type='button' onClick={() => removeTask(task.id)}>
-          remove
-        </button>
-      </li>
-    ))}
-  </ul>
-)
-
 const TaskList = ({tasks, removeTask}) => {
+  const emptyList = (
+    <div className={styles.emptyList}>
+      <p>You don't have any task</p>
+    </div>
+  )
+
+  const filledList = (
+    <ul className={styles.list}>
+      {tasks.map(task => (
+        <li className={styles.task} key={task.id}>
+          {task.name}
+
+          <button type='button' onClick={() => removeTask(task.id)}>
+            remove
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+
   if (tasks.length === 0) {
-    return <EmptyList />
+    return emptyList
   } else {
-    return <FilledList tasks={tasks} removeTask={removeTask} />
+    return filledList
   }
 }
 
