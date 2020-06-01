5bac9d058450c96f98197f2bc260bc62578f75aa
Jorge Arimitsu <jorge.developer@protonmail.com>
2020-05-31 21:12:24 -0300

    Creating fontawesome Library

---
+++ src/fontawesome.js
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faCheckCircle,
+  faCircle,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckCircle, faCircle, faTrashAlt)
