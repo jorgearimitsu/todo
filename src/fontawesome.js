97252c2f6cb6dc604b935f292f270d92c24c06ed
Jorge Arimitsu <jorge.developer@protonmail.com>
2020-05-31 21:43:04 -0300

    Refactoring fontawesome library

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,8 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircle } from '@fortawesome/free-regular-svg-icons'
 import {
   faCheckCircle,
-  faCircle,
-  faTrashAlt,
+  faTrashAlt
 } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faCheckCircle, faCircle, faTrashAlt)
