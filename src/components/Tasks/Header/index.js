fd7ca92b831686709ee8e271f51a54daf91a23c6
Jorge Arimitsu <jorge.developer@protonmail.com>
2020-06-04 17:56:48 -0300

    Refactoring Header component

--- src/components/Tasks/Header/index.js
+++ src/components/Tasks/Header/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import TaskCounter from './TaskCounter'
 import styles from './styles.module.css'
 
+
 const Header = ({tasks}) => {
+  const completedTasks = tasks.filter(task => task.done === true)
+  const remainingTasks = tasks.filter(task => task.done === false)
+
   return (
     <header>
       <h1 className={styles.title}>My Tasks</h1>
 
-      {tasks.length > 0 && (
-        <p className={styles.remaining}>
-          {tasks.length} {tasks.length === 1 ? 'task' : 'tasks'} remaining
-        </p>
-      )}
+      <div className={styles.taskInfo}>
+        <TaskCounter
+          className={styles.remaining}
+          tasks={remainingTasks}
+          text="remaining"
+        />
+        <TaskCounter
+          tasks={completedTasks}
+          text="completed"
+        />
+      </div>
+
     </header>
   )
 }
