a607abb814b7f9dacef74d3ddc87bc1f14d20d55
Jorge Arimitsu <jorge.developer@protonmail.com>
2020-06-04 19:49:39 -0300

    Adding ellipsisV icon

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,8 +2,15 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCircle } from '@fortawesome/free-regular-svg-icons'
 import {
   faCheckCircle,
+  faEllipsisV,
   faExclamationTriangle,
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheckCircle, faCircle, faExclamationTriangle, faTrashAlt)
+library.add(
+  faCheckCircle,
+  faCircle,
+  faEllipsisV,
+  faExclamationTriangle,
+  faTrashAlt
+)
